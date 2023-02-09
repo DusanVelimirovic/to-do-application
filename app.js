36075bab31546ce25ba0ce6c99d2275d90630b07
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2023-02-09 20:47:35 +0100

    install mongoose, connect to MongoDB Cluster, create Sceham for Item

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
+const Item = require("./models/Item");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
@@ -12,8 +16,44 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.set('strictQuery', false);
+
+//connect to mongoDB
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("Connected to Mongoose"))
+.catch(err => console.log(err));
+
+//create new Documents
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+  name: "Welcome to your todolist1!"
+});
+
+const item3 = new Item ({
+  name: "Welcome to your todolist3!"
+});
+
+const defaultItems = [item1, item2, item3];
+
+//insert new Documents into DB
+/*
+Item.insertMany(defaultItems, function(err){
+  if(err){
+    console.log(err);
+  } else {
+    console.log("Successfully saved default items to DB.");
+  }
+});*/
+
+Item.insertMany( defaultItems ).then(function(){
+  console.log("Data inserted")  // Success
+}).catch(function(error){
+  console.log("Data not inserted")      // Failure
+});
+
 
 app.get("/", function(req, res) {
 
