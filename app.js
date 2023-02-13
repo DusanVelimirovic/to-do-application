69109ae260684aab14ab1dc4c7de2fb75d6157d8
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2023-02-13 21:45:17 +0100

    changes

--- app.js
+++ app.js
@@ -142,11 +142,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if(port == null || port == ""){
-  port = 3000;
-}
 
-app.listen(port, function() {
-  console.log("Server has started");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
